870f7b91885513f175608307baf12446824b1c6d
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-15 18:28:16 +0300

    Update QuizContext.js

--- src/context/QuizContext.js
+++ src/context/QuizContext.js
@@ -84,7 +84,8 @@ function QuizProvider({ children }) {
     0
   );
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://github.com/BetulBuyukzengin/quizApi/blob/main/questions.json").
+    //fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
