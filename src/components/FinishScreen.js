1fa6feecd96962b41644f3d20e4f1043145753c4
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-07-13 20:11:41 +0300

    refactor: refactor

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -15,7 +15,7 @@ export default function FinishScreen() {
     <>
       <p className="result">
         <span>{emoji}</span>You scored <strong>{points} </strong>
-        out of {maxPossiblePoints}({Math.ceil(percentage)}%)
+        out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highScore} points)</p>
       <button
